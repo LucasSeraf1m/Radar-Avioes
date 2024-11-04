8592e88edfca46afabf78b287c577cf1c31e019c
Daniel Mafioleti <danielmafioleti2@gmail.com>
2024-11-04 09:59:44 -0300

    implementado busca de pares proximos de avioes

--- radar-avioes/src/app/funcao-avioes-prox/funcao-avioes-prox.component.ts
+++ radar-avioes/src/app/funcao-avioes-prox/funcao-avioes-prox.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataGridService } from '../Services/data-grid.service';
 
 @Component({
   selector: 'app-funcao-avioes-prox',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./funcao-avioes-prox.component.css']
 })
 export class FuncaoAvioesProxComponent {
+  constructor(private dataGridService: DataGridService) {}
 
+  distancia: number = 0;
+
+  paresProximos(){
+    let avioes = this.dataGridService.getLinhas();
+    let avioesProximos: any[] = [];
+
+    for(let i = 0; i < avioes.length; i++){
+      for(let j = i + 1; j < avioes.length; j++){
+        let distancia = Math.sqrt(Math.pow(avioes[i].X - avioes[j].X, 2) + Math.pow(avioes[i].Y - avioes[j].Y, 2));
+        if(distancia <= this.distancia){
+          avioesProximos.push({aviao1: avioes[i], aviao2: avioes[j], distancia: distancia});
+        }
+      }
+    }
+
+    avioesProximos.sort((a, b) => a.distancia - b.distancia);
+    
+    return avioesProximos;
+  }
 }
